383db540c6768f32df8bdcfe2e20987d70002144
aknafu12 <kgebre2021@gmail.com>
2023-12-02 11:57:34 +0300

    added the more function in views

--- SecureDjangoAuth/authentication/urls.py
+++ SecureDjangoAuth/authentication/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home , name='home')
+    path('', views.home , name='home'),
+    path('signup/', views.signup , name='signup'),
+    path('signin/', views.signin , name='signin'),
+    path('signout/', views.signout , name='signout'),
+
+
+
 ]
