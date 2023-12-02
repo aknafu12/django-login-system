be4ef5141ae44c7017a95f7c4d87f3271d365ee0
aknafu12 <kgebre2021@gmail.com>
2023-12-02 11:58:20 +0300

    defined function that render html pages

--- SecureDjangoAuth/authentication/views.py
+++ SecureDjangoAuth/authentication/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    return render(request,'authentication/index.html') #auth (dir name inside template
+
+def signup(request):
+    return render(request,'authentication/signup.html')
+
+def signin(request):
+    return render(request,'authentication/signin.html')
+def signout(request):
+    return render(request,'authentication/signout.html')
+
