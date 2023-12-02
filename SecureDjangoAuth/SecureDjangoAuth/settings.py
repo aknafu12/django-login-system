9858dc5e50c05cd189b2191c5bd85b9533712e42
aknafu12 <kgebre2021@gmail.com>
2023-12-02 11:20:17 +0300

    added postgresql database

--- SecureDjangoAuth/SecureDjangoAuth/settings.py
+++ SecureDjangoAuth/SecureDjangoAuth/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -73,12 +74,18 @@ WSGI_APPLICATION = 'SecureDjangoAuth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'SecureDjangoAuth', 
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost', 
+        'PORT': 5432,
     }
 }
+
 
 
 # Password validation
